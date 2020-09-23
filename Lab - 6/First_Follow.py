ee325e0ea035375cf90ccb3685f8a3b25b3360ac
Janardan Pethani <jpethani11@gmail.com>
2020-09-23 16:06:36 +0530

    Date - 23/6/2020

---
+++ Lab - 6/First_Follow.py
@@ -0,0 +1,82 @@
+import re
+first = []
+d_count = []
+def first_finder(key):
+    for k in re.split("/",production[key]):
+        i = 1
+        if re.split("||",k)[i] in production:
+            temp = first_finder(re.split("||",k)[i])
+            i += 1
+            while '#' in temp and not not re.split("||",k)[i] and re.split("||",k)[i] in production:
+               temp.remove('#')
+               temp = first_finder(re.split("||",k)[i])
+               i += 1
+            if re.split("||",k)[i] not in production and not not re.split("||",k)[i]:
+                first.remove('#')
+                if re.split("||",k)[i] not in first:
+                   first.append(re.split("||",k)[i])
+        else:
+           if re.split("||",k)[i] not in first:
+              first.append(re.split("||",k)[i])
+    return first
+def follow_finder(key):  ##S->Aa/BCD/e,A->CB/a,B->b/#,C->c/#,D->Ad/d/#       
+    follow = []
+    for i in production:
+        if key in production[i]:
+            for prod in re.split("/",production[i]):
+                if key in prod:
+                    j = 1
+                    key_index = re.split("||",prod).index(key)
+                    prodc = re.split("||",prod)
+                    #print("for",key)
+                    #print(first)
+                    if not not prodc[key_index+j] and prodc[key_index+j] in production:
+                        follow = first_finder(prodc[key_index+j])
+                        #print(prod,"first of 1",prodc[key_index+j],follow)
+                        j += 1
+                        while '#' in follow and not not prodc[key_index+j] and prodc[key_index+j] in production:
+                            follow.remove('#')
+                            follow = first_finder(prodc[key_index+j])
+                            #print(prod,"first of 2",prodc[key_index+j],follow)
+                            j += 1
+                        if not not prodc[key_index+j] and prodc[key_index+j] not in production:
+                            follow.remove('#')
+                            if prodc[key_index+j] not in follow:
+                                follow.append(prodc[key_index+j])
+                                #print(prod,"first of 3",prodc[key_index+j],follow)
+                        elif '#' in follow and not prodc[key_index+j]:
+                            if '#' in follow:
+                                follow.remove('#')
+                            if key == i:
+                                return follow
+                            follow.extend(follow_finder(i))
+                            follow = list(set(follow))
+                            #print(prod,"follow of",i,follow)
+                    elif not not prodc[key_index+j] and prodc[key_index+j] not in production:
+                        if prodc[key_index+j] not in follow:
+                            follow.append(prodc[key_index+j])
+                            #print(prod,"first of 4",prodc[key_index+j],follow)
+                    else:
+                        if key == i:
+                            return follow
+                        follow.extend(follow_finder(i))
+                        follow = list(set(follow))
+                        #print(prod,"follow of",i,follow)
+    global d_count
+    if not follow and (not d_count and key not in d_count)or(not not d_count and key in d_count):
+        follow = ['$']
+        d_count.append(key)
+    return follow
+    
+grammer = input("Enter all productions of grammer seperated by comma: ").split(",")
+production = {}
+for i in grammer:
+    production[re.split("->",i)[0]] = re.split("->",i)[1]
+print("Given Grammer:\n",production)
+for key in production:
+    key_first = first_finder(key)
+    print("First(",key,"):",key_first)
+    first.clear()
+    key_follow = follow_finder(key)
+    print("Follow(",key,"):",key_follow)
+    first.clear()    
--- Lab - 6/first.py
+++ Lab - 6/first.py
@@ -12,20 +12,26 @@ def find_term(rule):
     if rule[0] in Term:
         return rule[0]
     if rule[0] in nonTerm:
-        rules = productions[rule[0]]
-        to_return = []
-        for rule in rules:
-            if rule[0] in nonTerm:
-                to_return.extend(find_term(rule[0]))
-            elif rule[0] in Term:
-                to_return.append((rule[0]))
-        return to_return
+        returnLst = []
+        prodRules_i = productions[rule[0]]
+        for rule_1 in prodRules_i:
+            if rule_1[0] in Term:
+                returnLst.append(rule_1[0])
+            elif rule_1[0] in nonTerm:
+                temp_Lst = find_term(rule_1[0])
+                returnLst.extend(temp_Lst)
+            elif rule_1 == 'eps':
+                if len(rule) > 1:
+                    temp_Lst = find_term(rule[1])
+                    returnLst.extend(temp_Lst)
+                returnLst.append('eps')
+        return returnLst
 
-def f_follow(nonTerm,Term,productions):
+def f_first(nonTerm,Term,productions):
     for nT in nonTerm:
         rules = productions[nT]
         for rule in rules:
-            # print(nT,'->',rule)
+            # print(nT,'->',rule,end=' first: ')
             if rule == 'eps':
                 lst = first.get(nT, [])
                 lst.append('eps')
@@ -36,12 +42,13 @@ def f_follow(nonTerm,Term,productions):
                 first[nT] = lst
             else:
                 lst = first.get(nT, [])
-                lst1 = find_term(rule)
-                lst.extend(lst1)
+                lst_1 = find_term(rule)
+                lst_2 = list(dict.fromkeys(lst_1))
+                lst.extend(lst_2)
                 first[nT] = lst
-
+            print(first)
             
-f_follow(nonTerm,Term,productions)
+f_first(nonTerm,Term,productions)
 print(first)
 
 
